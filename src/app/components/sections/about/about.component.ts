36e7b3d1ae0cadf92d0958890db9ac6eca4db2d0
Leanne <sooyeunleanne@gmail.com>
2024-10-19 16:57:13 -0400

    styling about us

--- src/app/components/sections/about/about.component.ts
+++ src/app/components/sections/about/about.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   template: `
     <div class="about-container">
       <h1>About Us</h1>
-      <div>
+      <div class='paragraph-container'>
         <p>Fringles is a newly formed Korean youth band. With a passion for music and a heart for service, the band aims to fundraise for donation purposes, using their talent to support various charitable causes. Fringles plans to perform at different charity events, spreading joy and hope through their music while making a positive impact in their community and beyond.</p>
       </div>
     </div>

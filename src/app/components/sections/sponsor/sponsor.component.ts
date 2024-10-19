a6d55045ea49ae3386d867de1956924b6d031b30
Leanne <sooyeunleanne@gmail.com>
2024-10-19 12:57:04 -0400

    setting up the components

---
+++ src/app/components/sections/sponsor/sponsor.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sponsor',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      sponsor works!
+    </p>
+  `,
+  styles: ``
+})
+export class SponsorComponent {
+
+}
